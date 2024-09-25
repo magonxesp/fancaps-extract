a06bbf17d6001b2ea0453fdbb1b91f45ebd7279f
MagonxESP <janma.360@gmail.com>
2024-09-25 19:26:02 +0200

    ci: create bump version script

--- Makefile
+++ Makefile
@@ -1,28 +1,7 @@
 .PHONY: first-version bump-version
 
 first-version:
-	@if ! git show-ref --tags v0.0.0 --quiet; then \
-		git tag v0.0.0; \
-  	  	git cliff -o CHANGELOG.md; \
-  	  	git add CHANGELOG.md; \
-      	sed -i='' "s/version = \"v?[0-9.]+\.?[a-z]*\.?[0-9]*\"/version = \"0.0.0\"/g" build.gradle.kts; \
-      	git add build.gradle.kts; \
-      	sed -i='' -r "s/io\.github\.magonxesp:([a-z\-]+):v?[0-9.]+\.?[a-z]*\.?[0-9]*/io\.github\.magonxesp:\1:0.0.0/g" README.md; \
-      	git add README.md; \
-      	git commit -m "🚀 create first version"; \
-    fi
+	@python3 scripts/bump-version.py --first-version
 
 bump-version:
-	@VERSION=$$(git cliff --unreleased --bump --context | jq -r '.[0].version'); \
-	if [[ ! "$$VERSION" -eq "null" ]]; then \
-		git cliff --bump -o CHANGELOG.md; \
-		git add CHANGELOG.md; \
-		sed -i='' "s/version = \"v?[0-9.]+\.?[a-z]*\.?[0-9]*\"/version = \"$$VERSION\"/g" build.gradle.kts; \
-		git add build.gradle.kts; \
-		sed -i='' -r "s/io\.github\.magonxesp:([a-z\-]+):v?[0-9.]+\.?[a-z]*\.?[0-9]*/io\.github\.magonxesp:\1:$$VERSION/g" README.md; \
-		git add README.md; \
-		git commit -m "🚀 bump version to $$VERSION"; \
-		git tag v$$VERSION; \
-    else \
-    	echo "There is not a new version available"; \
-    fi
+	@python3 scripts/bump-version.py
---
+++ script/bump-version.py
@@ -0,0 +1,74 @@
+import argparse
+import subprocess
+import json
+import re
+
+parser = argparse.ArgumentParser(
+    prog='Bump version script',
+    description='Create new version number and add the git tag'
+)
+
+parser.add_argument('-f', '--first-version', dest='first_version', default=False, action='store_true')
+parser.add_argument('-a', '--alpha', default=False, action='store_true')
+parser.add_argument('-b', '--beta', default=False, action='store_true')
+
+args = parser.parse_args()
+
+version = None
+result = subprocess.run(['git', 'cliff', '--unreleased', '--bump', '--context'], capture_output=True)
+result.check_returncode()
+
+if result.stdout is not None:
+    context = json.loads(result.stdout.decode('utf-8'))
+    version = re.sub(r'^([0-9.]+\.?[a-z]*\.?[0-9]*)', r'v\1', context[0]['version'])
+
+if version is None:
+    print('There is not a new version available')
+    exit(0)
+
+if args.first_version is True and version is None:
+    version = 'v0.0.0'
+elif args.alpha is True:
+    version = re.sub(r'^(v?[0-9.]+)$', r'\1-alpha.1', version)
+elif args.beta is True:
+    version = re.sub(r'^(v?[0-9.]+)$', r'\1-beta.1', version)
+else:
+    version = re.sub(r'^(v[0-9.]+)(-[a-z]+\.?[0-9]*)$', r'\1', version)
+
+print(f'The new version is {version}')
+
+subprocess.run(['git', 'tag', version]).check_returncode()
+subprocess.run(['git', 'cliff', '--bump', '-o', 'CHANGELOG.md']).check_returncode()
+
+
+def replace_version(file_path, search, replacement):
+    with open(file_path, 'r+w') as f:
+        f.write(re.sub(search, replacement, f.read(), flags=re.MULTILINE))
+
+
+version_code = re.sub(r'^v', '', version)
+
+replace_version(
+    file_path='build.gradle.kts',
+    search=r'^version = "v?[0-9.]+\-?[a-z]*\.?[0-9]*"',
+    replacement=f'version = "{version_code}"'
+)
+
+replace_version(
+    file_path='README.md',
+    search=r'io\.github\.magonxesp:([a-z\-]+):v?[0-9.]+\-?[a-z]*\.?[0-9]*',
+    replacement=r'io\.github\.magonxesp:\1:' + version_code
+)
+
+files = [
+    'CHANGELOG.md',
+    'build.gradle.kts',
+    'README.md'
+]
+
+for file in files:
+    subprocess.run(['git', 'add', file]).check_returncode()
+
+subprocess.run(['git', 'commit', '-m', f'🚀 bump version to {version}']).check_returncode()
+print(f'🚀 bumped to version {version}')
+print('now you can run "git push"')
